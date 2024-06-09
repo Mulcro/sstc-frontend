51480e92fc71e9a2e240b48737c306d2a17f0b39
Mulero Alamou <Mulero.alamou@gmail.com>
2024-06-09 05:44:14 -0700

    minor modifications and tailwind initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Session from './Components/Session/session';
+import Admin from './Components/admin';
+import SideBar from './Components/sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <SideBar/>  */}
+      {/* <Admin/> */}
+      <Session/>
     </div>
   );
 }
