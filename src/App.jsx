b2749b16b98cc9ca80b83a92fee78a1279e83939
Mulero Alamou <Mulero.alamou@gmail.com>
2024-06-12 13:29:24 -0700

    Active session fetch now occurs here instead of in session comp

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,28 @@ import Session from './Components/Session/session';
 import Admin from './Components/admin';
 import SideBar from './Components/sidebar';
 import React from 'react';
+import {useDispatch} from "react-redux";
+import { fetchActiveSessions} from './State/Session/sessionSlice';
+import { useEffect } from 'react';
+
 
 function App() {
+
+  useEffect(() => {
+    const refreshActiveSessions = () => {
+      dispatch(fetchActiveSessions())
+    }
+
+    refreshActiveSessions();
+
+    const timerId = setInterval(refreshActiveSessions,2500)
+
+    return () => clearInterval(timerId)
+  },[])
+
+
+  const dispatch = useDispatch();
+  
   return (
     <div className="App">
       {/* <SideBar/>  */}
