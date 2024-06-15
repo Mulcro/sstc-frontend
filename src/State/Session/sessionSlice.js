e39fadeb20aac74607c163cf72e539f6c5efc7d9
Mulero Alamou <Mulero.alamou@gmail.com>
2024-06-15 00:52:06 -0700

    fixed issues that was causing unecessary re-renders

--- src/State/Session/sessionSlice.js
+++ src/State/Session/sessionSlice.js
@@ -1,39 +1,51 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import baseUrl from "../../baseUrl";
 
 const initialState = {
     loading: false,
     data: [],
-    error: ''
+    error: '',
 }
 
 export const fetchActiveSessions = createAsyncThunk(
     'sessions/fetchActiveSessions',
     async () => {
-        const res = await fetch(`http://localhost:5000/sessions/active`).then(res => res.json())
+        const res = await fetch(baseUrl + `/sessions/active`).then(res => res.json())
         return res.sessions;
     }
 )
+
+//All the if's are to prevent unecessary re-rendering as this slice is dispatched periodically every second
 const sessionSlice = createSlice({
     name:"sessions",
     initialState,
     reducers:{
         clearSessions: (state) => {
-            state.sessions = []
+            state.data = [];
         }
     },
     extraReducers: (builder) => {
+        //Was previouly updating the error to '' here but that was causing re-renders on every call and in the event of an error once it's set it's probably not going to change unless data is fetched and the error is resolved, which would result in the error being set to '' anyways
         builder.addCase(fetchActiveSessions.pending, (state) => {
-            state.loading = true;
+            if(!state.loading){
+                state.loading = true;
+            }
         })
         builder.addCase(fetchActiveSessions.fulfilled, (state,action) => {
-            state.loading = false;
-            state.data = action.payload
-            state.error = ''
+            if((state.error !== '') ){
+                state.error = '';
+            }
+            if(JSON.stringify(state.data) != JSON.stringify(action.payload)){
+                state.loading = false;
+                state.data = action.payload;
+            }
         })
         builder.addCase(fetchActiveSessions.rejected, (state,action) => {
-            state.loading = false;
-            state.sessions = [],
-            state.error = action.error.message
+            if((state.error !== action.error.message) ){
+                state.loading = false;
+                state.sessions = [],
+                state.error = action.error.message
+            }
         })
     }
 });
