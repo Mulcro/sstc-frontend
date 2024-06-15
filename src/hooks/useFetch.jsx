0a5d9dd351907000f572a7327517df774fb334c7
Mulero Alamou <Mulero.alamou@gmail.com>
2024-06-15 00:51:48 -0700

    fixed issues that was causing unecessary re-renders

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,27 +1,50 @@
 import {useState, useEffect} from 'react';
 
 const useFetch = (url) => {
-    const [data,setData]  = useState();
-    const [loading, setLoading] = useState();
-    const [error, setError] = useState();
+    const [data,setData]  = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
+    //Had to add state checks to prevent re-rendering as this function is periodically called every 2.5seconds
     useEffect(() => {
-        setLoading(true);
-        fetch(url)
-        .then(resp => {
-            if(resp.ok) {
-                return resp.json()
-            }
-            throw new Error('Network response was not ok')
-        })
-        .then(data => {
-            setData(data)
-            setLoading(false)
-        })
-        .catch(err => {
-            console.log(err)
-            setError(err)
-        })
+
+        let fetchTimer;
+
+        const fetchData = () => {
+            //To prevent unecessary state updates
+            if(!loading)
+                setLoading(true);
+
+            fetch(url)
+            .then(resp => {
+                if(resp.ok) {
+                    return resp.json()
+                }
+                return resp.json().then(err => new Error(err.message))
+            })
+            .then(dataa => {
+                setData(dataa)
+                setLoading(false)
+                setError(null)
+                console.log(`Data: ${dataa}`)
+                if(fetchTimer){
+                    clearInterval(fetchTimer)
+                }
+            })
+            .catch(err => {
+                //To prevent unecessary state updates
+                if(!(JSON.stringify(err.message) === JSON.stringify(error))){
+                    setLoading(false);
+                    setError(err.message);
+                }
+            })
+        }
+
+        fetchData();
+
+        fetchTimer = setInterval(fetchData,5000);
+
+        return () => clearInterval(fetchTimer);
     },[])
 
     return [data,loading,error]
